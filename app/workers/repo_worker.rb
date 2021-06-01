a71e14f6c3a064c6509c151297b3e241ff857394
Alex Darr <alexdarr@gmail.com>
2021-06-01 15:57:58 -0400

    fix argument error

--- app/workers/repo_worker.rb
+++ app/workers/repo_worker.rb
@@ -2,7 +2,7 @@ class RepoWorker
     include Sidekiq::Worker
     
     # Kick off the background job to process payload and protect the master branch
-    def perform
+    def perform(repo_params)
         puts repo_params 
     end
 end
